ec476f1d1e46997a0a8bc4635a951a78dc010c73
WillVinsonJMU <210252325+WillEVinson@users.noreply.github.com>
2026-09-09 21:06:44 -0400

    d

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+# consider adding save_as to more easily add instance that have similar values
+# you could do this for any or all of the below, e.g.
+# admin.site.register(Book, save_as=True)
+# except mayeb not bookinstance tho
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
